1f9ca4b4cf7f993659b241a87acf9113bd93e7b5
otili9890 <ocarm0809@gmail.com>
2023-11-06 13:44:27 +1000

    Updated for Vue Projects

--- Vue Example/main.js
+++ Vue Example/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { MankuIcon } from 'manku_icons';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(store);
+app.use(router);
+app.use(ElementPlus);
+app.use(VuePapaParse);
+app.component('font-awesome-icon', FontAwesomeIcon);
+app.component('manku-icon', MankuIcon)
