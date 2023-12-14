b821971546538f72958dc701c0a0e8a882e18d97
otili9890 <ocarm0809@gmail.com>
2023-12-14 13:01:31 +1100

    Lower casing inputs

--- dist/manku-icon-lib.js
+++ dist/manku-icon-lib.js
@@ -32,7 +32,7 @@ const y = (s, t) => {
     const t = c("notFound", a("Icon not found")), e = u({
       noIcon: !1
     }), n = s, r = l(
-      () => y(/* @__PURE__ */ Object.assign({ "../assets/icons/binderlink.svg": () => import("./binderlink-363c3a95.js"), "../assets/icons/conformsto.svg": () => import("./conformsto-1911bd71.js"), "../assets/icons/login.svg": () => import("./login-5fba9bbb.js"), "../assets/icons/loginplus.svg": () => import("./loginplus-aa585316.js"), "../assets/icons/public.svg": () => import("./public-c14cdd32.js"), "../assets/icons/spokenlanguage.svg": () => import("./spokenlanguage-85c1a282.js"), "../assets/icons/writtenlanguage.svg": () => import("./writtenlanguage-1b31e50e.js") }), `../assets/icons/${n.name}.svg`).then((o) => o, (o) => (e.noIcon = !0, {}))
+      () => (n.name.toLowerCase(), y(/* @__PURE__ */ Object.assign({ "../assets/icons/binderlink.svg": () => import("./binderlink-363c3a95.js"), "../assets/icons/conformsto.svg": () => import("./conformsto-1911bd71.js"), "../assets/icons/login.svg": () => import("./login-5fba9bbb.js"), "../assets/icons/loginplus.svg": () => import("./loginplus-aa585316.js"), "../assets/icons/public.svg": () => import("./public-c14cdd32.js"), "../assets/icons/spokenlanguage.svg": () => import("./spokenlanguage-85c1a282.js"), "../assets/icons/writtenlanguage.svg": () => import("./writtenlanguage-1b31e50e.js") }), `../assets/icons/${n.name}.svg`).then((o) => o, (o) => (e.noIcon = !0, {})))
     );
     return (o, k) => e.noIcon ? p(o.$slots, "notFound", { key: 0 }, () => [
       m(g(i(t)), 1)
